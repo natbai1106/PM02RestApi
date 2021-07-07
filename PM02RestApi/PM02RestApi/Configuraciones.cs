e855e5e317cb08bae53cdddac6e62bd8377ed2a1
anata <anata@DESKTOP-RTAADHS>
2021-07-06 18:28:09 -0600

    Agregar archivos de proyecto.

---
+++ PM02RestApi/PM02RestApi/Configuraciones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PM02RestApi
+{
+    public class Configuraciones
+    {
+        public const String IDFoursquare = "HR4HDOM0YPFEORMEFHTYHY5GZUWI5K2VKDT1VCQBPZW3IRTU";
+        public const String SecretFoursquare = "UB1O0POYIDCT2GUZTAG5L2CGG4LKLPNXLOGEI3R2S4TBT4Y4";
+        public const String apifoursquare = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+    }
+
+    public static class Sitios
+    {
+        public static String getUrl(Double latitud, Double longitud)
+        {
+            return String.Format (
+                Configuraciones.apifoursquare, 
+                latitud, 
+                longitud, 
+                Configuraciones.IDFoursquare, 
+                Configuraciones.SecretFoursquare, 
+                DateTime.Now.ToString("yyyyMMdd"));
+        }
+    }
+}
---
+++ PM02RestApi/PM02RestApi/MainPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Net.Http;
+using static PM02RestApi.Countries;
+using Newtonsoft.Json;
+using PM02RestApi;
+using System.Diagnostics;
+
+
+namespace PM02RestApi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnRestApi_Clicked(object sender, EventArgs e)
+        {
+            var coordenadas = Plugin.Geolocator.CrossGeolocator.Current;
+            var posicion = await coordenadas.GetPositionAsync();
+
+            list.ItemsSource = await AppClass.Metodos.getSites(posicion.Latitude, posicion.Longitude);
+
+
+            #region test
+            //var urlfq = Sitios.getUrl(platitud, plongitud);
+            //Debug.WriteLine(urlfq);
+
+            //var url = "https://restcountries.eu/rest/v2/region/europe";
+            //var urlfq = Sitios.getUrl(14.55, -88.05);
+
+            //using (HttpClient cliente = new HttpClient())
+            //{
+            //    var Respuesta = await cliente.GetAsync(url);
+
+            //    if (Respuesta.IsSuccessStatusCode)
+            //    {
+            //        var json = Respuesta.Content.ReadAsStringAsync().Result;
+            //        await DisplayAlert("Respuesta", json, "Ok");
+            //        var Paises = JsonConvert.DeserializeObject<List<CountriesREst>>(json);
+            //    }
+            //}
+            #endregion
+        }
+    }
+}
